6a786de1820acf379982145e0e602fffc80cb4ad
jordi <jordi@192.168.19.136>
2018-02-14 18:23:13 +0100

    CU01

---
+++ resources/views/CU01_login.blade.php
@@ -0,0 +1,42 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="{{ url('/assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+ </head>
+ <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <form class="form-horizontal" method="POST" action="">
+                            {{ csrf_field() }}
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email" placeholder="Username" value="" required autofocus>
+                                </div>
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
+                                </div>
+                            <div class="form-group">
+                                <div class="col-md-8 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Iniciar Sesion
+                                    </button><br>
+                                    <a class="btn btn-link" href="">
+                                        ¿Olvidaste tu contraseña?
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="{{ url('/assets/bootstrap/js/bootstrap.min.js') }}"></script>
+  </body>
+</html>
