e18e3be96e28c2c4b38d5fea56ded2551071db4f
jordi <jordi@192.168.19.136>
2018-02-14 19:17:48 +0100

    CU01_03

--- resources/views/CU01_login.blade.php
+++ resources/views/CU01_login.blade.php
@@ -42,6 +42,7 @@
         </div>
     </div>
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+   
     <script src="{{ url('/assets/bootstrap/js/bootstrap.min.js') }}"></script>
 	
   </body>
