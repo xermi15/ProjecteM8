461d651b2b7c4ad9250812bfd7dad8c542519fa6
jordi <jordi@192.168.19.136>
2018-02-21 16:45:59 +0100

    login2

--- resources/views/CU01_login.blade.php
+++ resources/views/CU01_login.blade.php
@@ -6,44 +6,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login</title>
 
-    <link href="{{ url('/assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ url('/css/bootstrap.min.css') }}" rel="stylesheet">
 
 
   </head>
   <body>
     <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        <form class="form-horizontal" method="POST" action="">
-                            {{ csrf_field() }}
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <br><br>
+            <div class="panel panel-default">
+                
 
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control" name="email" placeholder="Username" value="" required autofocus>
-                                </div>
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
-                                </div>
-                            <div class="form-group">
-                                <div class="col-md-8 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Iniciar Sesion
-                                    </button><br>
-                                    <a class="btn btn-link" href="">
-                                        ¿Olvidaste tu contraseña?
-                                    </a>
-                                </div>
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="CU01_login">
+                        {{ csrf_field() }}
+                        
+                        <div class="panel-heading col-md-offset-1"><h1>Iniciar Sesión</h1></div>
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-2">
+                                <input id="user" type="user" class="form-control" name="user" placeholder="Username" value="" required autofocus>
                             </div>
-                        </form>
-                    </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-2">
+                                <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-8">
+                                <button type="submit" class="btn btn-primary col-md-offset-6">
+                                    Iniciar Sesión
+                                </button>
+                                <br>
+
+                                <!--<a class="btn btn-link col-md-offset-4" href="">
+                                    ¿Olvidaste tu contraseña?
+                                </a>-->
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+</div>
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
    
-    <script src="{{ url('/assets/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ url('/js/bootstrap.min.js') }}"></script>
 	
   </body>
 </html>
