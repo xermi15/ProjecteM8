104680ea377225bc259c37341416efbe84f2e74e
issamm6 <issam.996@hotmail.com>
2018-03-07 20:00:05 +0100

    commitPrueba10

--- resources/views/CU_26.blade.php
+++ resources/views/CU_26.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.CU_02')
+@extends('layouts.master')
 @section('content')
 <div class="container">
   <h2>Crear Plantilla Workflow</h2>
@@ -6,15 +6,15 @@
     <div class="form-group">
       <label class="control-label col-sm-2" for="Nombre">Nombre:</label>
       <div class="col-sm-10">
-        <input type="email" class="form-control" id="Nombre" placeholder="Enter email" name="email">
+        <input type="email" class="form-control" id="Nombre" placeholder="Introduce descripcion" name="email">
       </div>
     </div>
-      <div class="form-group">
+      <!--<div class="form-group">
           <label class="control-label col-sm-2" for="title">Documento</label>
           <div class="col-sm-10">
           <input type="file" name="documento" id="arxiu" class="form-control" required>
           </div>
-      </div>
+      </div>-->
        <!-- <php /*
         Al hacer click encima a seleccionar tiene que mostrar los revisores
       $sentencia="select * from Revisors order by nomRevisor asc";
@@ -33,20 +33,35 @@
       <div class="form-group">
           <label class="control-label col-sm-2" for="title">Revisor/es</label>
           <div class="col-sm-10">
-          <input type="file" name="revisor" id="revisor" class="form-control" required>
+          <!--<input type="file" name="revisor" id="revisor" class="form-control" required>-->
+                <select class="form-control col-sm-10">
+                        <option selected></option>
+                        <option>Jorge</option>
+                        <option>Issam</option>
+                        <option>Fede</option>
+                        <option>Esther</option>
+                </select>
           </div>
       </div>
+     
+     
+     
       <!-- Al hacer click encima a seleccionar tiene que mostrar los aprovadores -->
       <div class="form-group">
-          <label class="control-label col-sm-2" for="title">Aprovador/es</label>
+          <label class="control-label col-sm-2" for="title">Aprovador</label>
           <div class="col-sm-10">
-          <input type="file" name="daprovador" id="aprovador" class="form-control" required>
+          <!--<input type="file" name="daprovador" id="aprovador" class="form-control" required>-->
+              <select class="form-control col-sm-10">
+                        <option selected></option>
+                        <option>Fede</option>
+                        <option>Esther</option>
+                </select>
           </div>
       </div>
       <div class="form-group">
 	<select class="form-control col-sm-10">
       <option>Guardar Plantilla</option>
-      <option>Crear Workflow</option>
+      <option >Crear Workflow</option>
       <option>Crear Workflow y Guardar Plantilla</option>
         </select></div>
           
@@ -61,11 +76,11 @@
         <button type="button" name="OcultarPlantilla" class="btn btn-default">Ocultar Plantilla</button>
       </div>
          <br>
-    <div class="form-group">        
+    <!--<div class="form-group">        
       <div class="col-sm-offset-2 col-sm-10">
         <button type="submit" class="btn btn-default">Submit</button>
       </div>
-    </div>
+    </div>-->
   </form>
 </div>
 @stop
