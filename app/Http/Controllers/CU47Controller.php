99e6c886bbb1100a2a151bfb8c53017c5e02ad46
Aleix <Aleix@DESKTOP-5URUF4M>
2018-03-15 18:17:34 +0100

    Aleix16

---
+++ app/Http/Controllers/CU47Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Usuari;
+use Illuminate\Http\Request;
+
+class CU47Controller extends Controller {
+        public function getIndex($id){
+//             $usuari = Usuari::All();
+//             return redirect('CU_42');
+//          return view('CU_42');
+             return view('CU_47', array('DadesUsuari'=>Usuari::findOrFail($id)));
+
+
+        }
+}
