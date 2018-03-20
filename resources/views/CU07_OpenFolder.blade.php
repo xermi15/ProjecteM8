af2f6a7c970014d296af3cf5ed6b429353cfeb08
sergi <sergi@DESKTOP-7M7RV96>
2018-03-20 19:25:55 +0100

    Modal Eliminar Archivo/Carpeta

--- resources/views/CU07_OpenFolder.blade.php
+++ resources/views/CU07_OpenFolder.blade.php
@@ -33,7 +33,7 @@
                 <td class="col-md-1"><span class="glyphicon glyphicon-lock"></td>
                 <td class="col-md-1"><span class="glyphicon glyphicon-wrench"></td>
                 <td class="col-md-1"><span class="glyphicon glyphicon-new-window"></td>
-                <td class="col-md-1"><span class="glyphicon glyphicon-trash"></td>
+                <td class="col-md-1"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#borrarModal" data-book-id="{{$carpeta->nom}}"><span class="glyphicon glyphicon-trash"></button></td>
             </tr>
             @endforeach
             @foreach( $arxius as $key => $document)
@@ -47,8 +47,36 @@
                 <td class="col-md-1"><span class="glyphicon glyphicon-paperclip"></td>
                 <td class="col-md-1"><span class="glyphicon glyphicon-list-alt"></td>
                 <td class="col-md-1"><span class="glyphicon glyphicon-new-window"></td>
-                <td class="col-md-1"><span class="glyphicon glyphicon-trash"></td>
+                <td class="col-md-1"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#borrarModal" data-book-id="{{$document->nom}}"><span class="glyphicon glyphicon-trash"></button></td>
             </tr>
             @endforeach
         </table>
+
+        <!-- Modal -->
+        <div class="modal fade" id="borrarModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h4 class="modal-title" id="exampleModalLabel">Eliminar</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                  Seguro que desea eliminar <b><p id="bookId" style="display: inline-block"></p></b>?
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-primary btn-danger">Eliminar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <script>
+            $('#borrarModal').on('show.bs.modal', function(e) {
+                var bookId = $(e.relatedTarget).data('book-id');
+                $('#bookId').text(bookId);
+            });
+        </script>
 @stop
