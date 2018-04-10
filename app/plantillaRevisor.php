7bf254f1aaeafe316c721453c3fc434044980740
issamm6 <issam.996@hotmail.com>
2018-04-10 18:21:07 +0200

    commitPruebac26bdkjjj

---
+++ app/plantillaRevisor.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class plantillaRevisor extends Model
+{
+    protected $table = 'plantillarevisors';
+    protected $primaryKey  = ['idPlantilla', 'idUsuariRevisor'];
+    public $timestamps = false;
+}
