9e8aa61f060fd3a17e1adf8d3560e93db73796a4
Oli <Oli@Oli-VAIO>
2018-04-11 16:18:43 +0200

    CU_45, CU_43, CU_52 Controllers

--- app/Http/Controllers/CU_45Controller.php
+++ app/Http/Controllers/CU_45Controller.php
@@ -3,69 +3,83 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Usuari;
+use App\UsuariGrup;
+use App\Grup;
 use Carbon\Carbon;
 use Krucas\Notification\Facades\Notification;
-use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\DB;
 
 class CU_45Controller extends Controller {
 
     public function mostraUsuari() { // metode provisional per fer proves
-        $user = Usuari::where('idUsuari', 5)->first();
-        return view('CU_45')->with('usuari', $user);
-    }
+        $id = 5;
+        $user = Usuari::where('idUsuari', $id)->first();
+        $user_grup = UsuariGrup::where('idUsuari', $id)->get();
+        //$user_grup = DB::select("SELECT * FROM usuariGrup WHERE idUsuari = " . $id . "");
 
-    /* public function mostraUsuari($idUsuari) { // metode correcte pero ha de rebre id d'altre cas d'us perque funcioni
-      $user = Usuari::where('idUsuari', $id)->first();
-      return view('CU_45')->with('usuari', $user);
-      } */
+        $grups = collect([]);
+        foreach ($user_grup as $u) {
+            $grups = $grups . Grup::where('idGrup', $u->idGrup)->first();
+        }
+
+        /* foreach ($grups as $g) {
+          $nom_grups = $nom_grups . $g->nom;
+          } */
+
+        if ($user_grup == null) {
+            $user_grup = 0;
+        }
+        return view('CU_45_ModificarUsuari')->with('usuari', $user)
+                        ->with('usuari_grup', $user_grup)
+                        //->with('nom_grup', $nom_grups);
+                        ->with('nom_grup', $grups);
+    }
 
     public function modificarUsuari(Request $request) { //  metode provisional per fer proves
         //public function modificarUsuari(Request $request, $id) { // metode correcte pero ha de rebre id d'altre cas d'us perque funcioni
         //$date = Carbon::now(); // sol es fará servir si modifiquem la dataModificació
-        $usuari = Usuari::where('email', $request->email) // si ja existeix l'usuari o email no deixa crearlo
-                        ->orwhere('nomUsuari', $request->nomUsuari)->first();
+        $id = 5;
+        /* $usuari = Usuari::where('idUsuari', '<>', $id)
+          ->where('nomUsuari', $request->nomUsuari)
+          ->orWhere('email', $request->email)->first(); */
 
-        $user = Usuari::where('idUsuari', 5)->first(); //  metode provisional per fer proves
-        //$user = Usuari::findOrFail($idUsuari); // metode correcte pero ha de rebre id d'altre cas d'us perque funcioni
+        /* $usuari = Usuari::where('nomUsuari', $request->nomUsuari)
+          ->orWhere('email', $request->email)->first(); */
 
 
-        /*         * ***************** INTENTANDO VALIDAR EMAIL PARA QUE NO LO TENGA EN CUENTA**************** */
-        /* Validator::make($data, [
-          'email' => [
-          'required',
-          Rule::unique('usuaris')->ignore($usuari->email, 'idUsuari'),
-          ],
-          ]); */
+
+        $usuari = DB::select("SELECT * FROM usuaris WHERE idUsuari <> " . $id . " AND (nomUsuari = '" . $request->nomUsuari . "' OR email = '" . $request->email . "')");
 
         /* $this->validate($request, [
           'email' => [
           'required',
-          Rule::unique('usuaris')->ignore($usuari->email, 'idUsuari'),
+          Rule::unique('usuaris')->ignore($user->id, 'idUsuari'),
           ],
           ]); */
-        /*         * ********************************************************************************************* */
+
+        $user1 = Usuari::where('idUsuari', $id)->first(); //  metode provisional per fer proves
+        //$user1 = Usuari::findOrFail($idUsuari); // metode correcte pero ha de rebre id d'altre cas d'us perque funcioni
 
         if ($usuari == null) {
 
-            $user->nomUsuari = $request->nomUsuari;
-            $user->contrasenya = bcrypt($request->contrasenya);
-            $user->nom = $request->nom;
-            $user->cognoms = $request->cognoms;
-            $user->email = $request->email;
-            $user->dadesPostals = $request->dadesPostals;
+            $user1->nomUsuari = $request->nomUsuari;
+            $user1->contrasenya = bcrypt($request->contrasenya);
+            $user1->nom = $request->nom;
+            $user1->cognoms = $request->cognoms;
+            $user1->email = $request->email;
+            $user1->dadesPostals = $request->dadesPostals;
             //$user->dataModificacio = $date; // sol es fará servir si modifiquem la dataModificació
-            $user->estat = $request->estat;
-            $user->tipus = $request->tipus;
-            $user->save();
+            $user1->estat = $request->estat;
+            $user1->tipus = $request->tipus;
+            $user1->save();
 
             Notification::success("L'usuari s'ha modificat correctament.");
-            return redirect('CU_45');
+            return redirect('CU_45_ModificarUsuari');
             //return redirect('/mostrarusuari/' . ($id));  //exemple per si es vol mostrar l'usuari q s'acaba de crear
         } else {
             Notification::error("Error!!! Aquest usuari ja existeix.");
-            return back()->withInput();
+            return redirect('CU_45_ModificarUsuari');
         }
     }
 
