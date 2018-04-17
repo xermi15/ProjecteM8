c052b016a8d705f694069f7a7cd5d401fec7f748
David Roig <roig_96@hotmail.com>
2018-04-18 00:17:07 +0200

    Fer modal Pujar Versió

--- resources/views/CU07_OpenFolder.blade.php
+++ resources/views/CU07_OpenFolder.blade.php
@@ -53,7 +53,7 @@
                 <td class="col-md-1"><span class="glyphicon glyphicon-info-sign"></span></td>
                 <td class="col-md-1"><span class="glyphicon glyphicon-link"></span></td>
                 <td class="col-md-1"><span class="glyphicon glyphicon-cloud-download"></td>
-                <td class="col-md-1"><span class="glyphicon glyphicon-paperclip"></td>
+                <td class="col-md-1"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pujarVersio"><span class="glyphicon glyphicon-paperclip"></span></button></button></td>
                 <td class="col-md-1"><span class="glyphicon glyphicon-list-alt"></td>
                 <td class="col-md-1"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#moureDocumentModal" data-book-id="{{$document->idDocument}}" data-book-name="{{$totesCarpetes}}"><span class="glyphicon glyphicon-new-window"></button></td>
                 <td class="col-md-1"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#borrarModal" data-book-id="{{$document->idDocument." documento"}}" data-book-name="{{$document->nom}}"><span class="glyphicon glyphicon-trash"></button></td>
@@ -376,6 +376,58 @@
                                     </button>
                                 </div>
                             </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Pujar Versió -->
+        <div class="modal fade" id="pujarVersio" role="dialog">
+            <div class="modal-dialog">
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <div>
+                            <h3 class="panel-title text-center">
+                                <span class="glyphicon glyphicon-file" aria-hidden="true"></span>
+                                Pujar Versió
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="modal-body">
+                        <form method="POST" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                            <input hidden name="id" value="{{$document->idDocument}}">
+                            <!-- <input hidden name="versioOld" value="{{$document->versioInterna}}"> -->
+
+                            <div class="form-group">
+                                <label for="title">Nom del arxiu</label>
+                                <input type="text" name="nom" id="nom" class="form-control" required value="{{$document->nom}}">
+                            </div>
+
+                            <div class="form-group">
+                                    <label for="title">Ruta de l'arxiu</label>
+                                    <input type="file" name="arxiu" id="arxiu" class="form-control" required>
+                            </div>
+
+                            <div class="form-group">
+                                    <label for="synopsis">Descripció (Opcional)</label>
+                            <textarea name="desc" id="desc" class="form-control" rows="3" value="{{$document->descripcio}}"></textarea>
+                            </div>
+
+                            <div class="form-group">
+                                    <label for="synopsis">Versió</label>
+                                    <input pattern="^[0-9]*(\.[0-9]+)*$" type="text" name="ver" id="ver" class="form-control">
+                            </div>
+
+                            <div class="form-group text-center">
+                                    <button type="submit" class="btn btn-primary">
+                                            Pujar arxiu 
+                                    </button>
+                            </div> 
                         </form>
                     </div>
                     <div class="modal-footer">
