f92ed85c0f93d824e030c16dcf7834b90f2a00e7
jorgekbyo <jorgegonzalezcabello@gmail.com>
2018-04-19 02:36:50 +0200

    Commit Issam Jorge

--- resources/views/CU_50.blade.php
+++ resources/views/CU_50.blade.php
@@ -4,11 +4,12 @@
   <h2>Mostrar Plantilla Workflow</h2>
   <table class="table">
             <thead>
-                <tr>
+                <tr> 
                     <th>ID Plantilla</th>
                     <th>Nom Plantilla</th>
-                    <th>ID UsuariCreador</th>
-                    <th>ID UsuariAprovador</th>
+                    <th>UsuariCreador</th>
+                    <th>UsuariAprovador</th> 
+                    <th>UsuariRevisor </th>
                     <th>Opcions</th>
                 </tr>
             </thead>
@@ -19,14 +20,38 @@
                     <tr>
                         <td>{{ $plantillas->idPlantilla }}</td>
                         <td>{{ $plantillas->nomPlantilla }}</td>
-                        <td>{{ $plantillas->idUsuariCreador }} </td>
-                        <td>{{ $plantillas->idUsuariAprovador }}</td>
-                        <td><a href="{{url('CU_27_EditarPlantilla' )}}" class="btn btn-warning btn-lg active" role="button"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>Editar Pelicula</a>
+                        
+                        
+                        @foreach($users as $user)
+                        @if ($plantillas->idUsuariCreador == $user->idUsuari)
+                        <td>{{ $user->nomUsuari }}</td>
+                        @endif
+                        @endforeach
+                        
+                        @foreach($users as $user)
+                        @if ($plantillas->idUsuariAprovador == $user->idUsuari)
+                        <td>{{ $user->nomUsuari }}</td>
+                        @endif
+                        @endforeach
+                        
+                        @foreach($plantillarevisors as $revi)
+                        
+                        @foreach($users as $user)
+                        @if ($plantillas->idPlantilla == $revi->idPlantilla)
+                            @if($revi->idUsuariRevisor==$user->idUsuari)
+                        <td >{{ $user->nomUsuari }}</td>
+                        @endif
+                        @endif
+                        @endforeach
+                        @endforeach
+                        
+                        <td><a href="{{url('CU_27_EditarPlantilla' )}}" class="btn btn-warning btn-lg active" role="button"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
             
-            <a href="{{url('CU_28_EliminarPlantilla' )}}" class="btn btn-danger btn-lg active" role="button"><span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span>Eliminar Pelicula</a>
+            <a href="{{url('CU_28_EliminarPlantilla' )}}" class="btn btn-danger btn-lg active" role="button"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>
             </td>
                     </tr>
                 @endforeach
+               
             </tbody>
 
         </table>
