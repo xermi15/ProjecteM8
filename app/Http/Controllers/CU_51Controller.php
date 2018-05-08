80c8a6e1cfc736d5b71af9aad6f1edeeae1f94c4
jordi <jordi@192.168.19.188>
2018-05-08 18:07:59 +0200

    logs

--- app/Http/Controllers/CU_51Controller.php
+++ app/Http/Controllers/CU_51Controller.php
@@ -14,7 +14,7 @@ class CU_51Controller extends Controller
         
        $log = new Logs;
        $log->idUsuari= $_SESSION['idUsuari'];
-       $log->descripcio="L'usuari ".$_SESSION['idUsuari']." a tancat sessio";
+       $log->descripcio="L'usuari ".$_SESSION['nomUsuari']." a tancat sessio";
        $log->dataLog=date('Y-m-d');
        $log->hora = date('H:i:s');
        $log->path = " ";
