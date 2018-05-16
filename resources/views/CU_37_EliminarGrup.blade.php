f259b9bdfe2caba75040d0a119a5a3cefe59a7ac
Oscar Robles <oscarrobles27@gmail.com>
2018-05-17 00:24:47 +0200

    Revert "correcio disseny (eliminar i modificar grup)"
    This reverts commit 3b5c1dba7a819ccc82f769bcce169a785876c1db.

--- resources/views/CU_37_EliminarGrup.blade.php
+++ resources/views/CU_37_EliminarGrup.blade.php
@@ -1,15 +1,13 @@
 <button id="deleteBtn" type="button" onclick="guardaidGrup_idUsuarisGrup(this);" data-toggle="modal" data-target="#myModal_3" style="padding: 0; border: none; background: none; margin-left: 20px; margin-right: 20px;">
     <i class="glyphicon glyphicon-trash"></i>
 </button>
-
-<div class="modal fade" tabindex="-1" role="dialog" id="myModal_3">
-
-    <div id="modal_eliminar" class="modal-dialog" role="document">
-        <div class="modal-content">
-            <form id="formDelGrup" name="formDelGrup" class="form-horizontal" method="POST" action="{{ url('/delGrup') }}">
-                <div class="controls">
-                    {{ method_field('PUT') }}
-                    {{ csrf_field() }}
+<form id="formDelGrup" name="formDelGrup" class="form-horizontal" method="POST" action="{{ url('/delGrup') }}">
+    <div class="controls">
+        {{ method_field('PUT') }}
+        {{ csrf_field() }}
+        <div class="modal fade" tabindex="-1" role="dialog" id="myModal_3">
+            <div id="modal_eliminar" class="modal-dialog" role="document">
+                <div class="modal-content">
                     <div class="modal-header" style="text-align: center; background: #455A64; color: white; border-radius: 5px 5px 0px 0px;">
                         <h4 class="modal-title" style="text-align: center; display:inline; cursor:default;">Eliminar grup</h4>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close" style="display:inline;">
@@ -17,13 +15,10 @@
                         </button>
                     </div>
                     <div class="modal-body-eliminarGrup">
-
                         <h4>Segur que desitja eliminar el següent grup?</h4>
                         <!--<input type="text" name="nombre_grupo" id="nombre_grupo" style="cursor: default; border: none; background: none;" disabled >-->
                         <label for="password">Contrasenya:</label>
-                        <input type="password" name="password" id="password" size="35" placeholder="Contrasenya administrador grup" required>
-                        <h6>(funcio control confirmacio per contrasenya per desenvolupar)</h6>
-
+                        <input type="password" name="password" id="password" size="35" placeholder="Contrasenya del administrador del grup" required>
                     </div>
                     <div class="modal-footer" style="text-align: center;">
                         <button type="submit" class="btn btn-danger" id="modalEliminar" style="margin-right: 25%;">
@@ -34,8 +29,7 @@
                         </button>
                     </div>
                 </div>
-            </form>  
+            </div>
         </div>
     </div>
-
-</div>
+</form>
